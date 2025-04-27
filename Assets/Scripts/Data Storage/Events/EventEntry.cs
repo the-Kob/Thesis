ba291a6061749c818e35661ca76ccc9ffa713803
Guilherme Pereira <gcn.pereira@campus.fct.unl.pt>
2025-04-27 01:10:28 +0100

    Created Event Factory to produce Event Entries

---
+++ Assets/Scripts/Data Storage/Events/EventEntry.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Data_Storage.Events {
+    public class EventEntry
+    {
+        public int StudyId { get; set; }
+        public int Score { get; set; }
+        public int Combo { get; set; }
+        public EventType EventType { get; set; }
+        public Agent Actuator { get; set; }
+        public Agent Receiver { get; set; }
+        public int ElapsedTime { get; set; }
+
+        public Dictionary<string, string> ToDictionary()
+        {
+            return new Dictionary<string, string>
+            {
+                {"study_id", StudyId.ToString()},
+                {"score", Score.ToString()},
+                {"combo", Combo.ToString()},
+                {"event_type", EventType.ToString()},
+                {"event_actuator", Actuator == Agent.None ? "-" : Actuator.ToString()},
+                {"event_receiver", Receiver == Agent.None ? "-" : Receiver.ToString()},
+                {"elapse_time", ElapsedTime.ToString()}
+            };
+        }
+    }
+}
---
+++ Assets/Scripts/Data Storage/Events/EventFactory.cs
@@ -0,0 +1,127 @@
+using UnityEngine;
+
+namespace Data_Storage.Events
+{
+    public enum EventType
+    {
+        Buff,
+        Nerf,
+        SecondaryAttack,
+        GettingHit,
+        End,
+        EnemyHit,
+        EnemyKill,
+        BulletMiss
+    }
+
+    public enum Agent
+    {
+        None,
+        P1,
+        P2,
+        P1Enemy,
+        P2Enemy
+    }
+    
+    public static class EventFactory
+    {
+        public static EventEntry CreateEffectEvent(bool isP1, int elapsedTime, int effect, int studyId, int score, int combo)
+        {
+            return new EventEntry
+            {
+                StudyId = studyId,
+                Score = score,
+                Combo = combo,
+                EventType = effect % 2 == 0 ? EventType.Buff : EventType.Nerf,
+                Actuator = isP1 ? Agent.P1 : Agent.P2,
+                Receiver = effect < 2 ? Agent.P2Enemy : Agent.P1Enemy,
+                ElapsedTime = elapsedTime
+            };
+        }
+
+        public static EventEntry CreateGettingHitEvent(bool isP1, int elapsedTime, int studyId, int score, int combo)
+        {
+            return new EventEntry
+            {
+                StudyId = studyId,
+                Score = score,
+                Combo = combo,
+                EventType = EventType.GettingHit,
+                Actuator = isP1 ? Agent.P1Enemy : Agent.P2Enemy,
+                Receiver = isP1 ? Agent.P1 : Agent.P2,
+                ElapsedTime = elapsedTime
+            };
+        }
+
+        public static EventEntry CreateSecondaryAttackEvent(bool isP1, int elapsedTime, int studyId, int score, int combo)
+        {
+            return new EventEntry
+            {
+                StudyId = studyId,
+                Score = score,
+                Combo = combo,
+                EventType = EventType.SecondaryAttack,
+                Actuator = isP1 ? Agent.P1 : Agent.P2,
+                Receiver = isP1 ? Agent.P1Enemy : Agent.P2Enemy,
+                ElapsedTime = elapsedTime
+            };
+        }
+        
+        public static EventEntry CreateBulletMissEvent(bool isP1, int elapsedTime, int studyId, int score, int combo)
+        {
+            return new EventEntry
+            {
+                StudyId = studyId,
+                Score = score,
+                Combo = combo,
+                EventType = EventType.BulletMiss,
+                Actuator = isP1 ? Agent.P1 : Agent.P2,
+                Receiver = isP1 ? Agent.P1Enemy : Agent.P2Enemy,
+                ElapsedTime = elapsedTime
+            };
+        }
+
+        public static EventEntry CreateEndEvent(int score, int studyId, int combo)
+        {
+            return new EventEntry
+            {
+                StudyId = studyId,
+                Score = score,
+                Combo = combo,
+                EventType = EventType.End,
+                Actuator = Agent.None,
+                Receiver = Agent.None,
+                ElapsedTime = 180
+            };
+        }
+        
+        public static EventEntry CreateEnemyHitEvent(bool isP1, int elapsedTime, int studyId, int score, int combo)
+        {
+            return new EventEntry
+            {
+                StudyId = studyId,
+                Score = score,
+                Combo = combo,
+                EventType = EventType.EnemyHit,
+                Actuator = isP1 ? Agent.P1 : Agent.P2,
+                Receiver = isP1 ? Agent.P1Enemy : Agent.P2Enemy,
+                ElapsedTime = elapsedTime
+            };
+        }
+
+        public static EventEntry CreateEnemyKillEvent(bool isP1, int elapsedTime, int studyId, int score, int combo)
+        {
+            return new EventEntry
+            {
+                StudyId = studyId,
+                Score = score,
+                Combo = combo,
+                EventType = EventType.EnemyKill,
+                Actuator = isP1 ? Agent.P1 : Agent.P2,
+                Receiver = isP1 ? Agent.P1Enemy : Agent.P2Enemy,
+                ElapsedTime = elapsedTime
+            };
+        }
+    }
+
+}
